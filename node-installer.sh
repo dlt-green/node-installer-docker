c82f3aa27f3a309b6b94f86ac6892dc4070859f9
dlt.green <contact@dlt.green>
2022-06-21 00:03:05 +0200

    Update node-installer.sh

--- node-installer.sh
+++ node-installer.sh
@@ -1,145 +1,222 @@
-
 #!/bin/sh
 
-rm -r node-installer.sh
-clear
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║            DLT.GREEN AUTOMATIC SHIMMER INSTALLATION WITH DOCKER             ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+function MainMenu {
+	clear
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║               DLT.GREEN AUTOMATIC NODE-INSTALLER WITH DOCKER                ║"
+	echo "║                                                                             ║"
+	echo "║                              1. System Updates                              ║"
+	echo "║                              2. Docker Installation                         ║"
+	echo "║                              3. IOTA Mainnet                                ║"
+	echo "║                              4. IOTA Devnet                                 ║"
+	echo "║                              5. SHIMMER Mainnet                             ║"
+	echo "║                              6. SHIMMER EVM                                 ║"
+	echo "║                                                                             ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo "select: "
+	echo ""
 
-read -p 'Press [Enter] key to continue...' W
+	read n
+    case $n in
+	1) SystemUpdates ;;
+	2) Docker ;;
+	3) MainMenu ;;
+	4) MainMenu ;;
+	5) ShimmerMainnet ;;
+	6) MainMenu ;;
+	*) exit ;;
+	esac
+}
 
-cd /var/lib/hornet
-docker-compose down
-sudo apt-get install jq -y
+function SystemUpdates {
+	clear
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                     DLT.GREEN AUTOMATIC SYSTEM UPDATES                      ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║   Update and install packages to allow apt to use a repository over HTTPS   ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	read -p 'Press [Enter] key to continue...' W
 
-sudo apt-get update
+	clear
+	sudo apt-get update && apt-get upgrade -y
+	sudo apt-get dist-upgrade -y
+	sudo apt upgrade -y
+	sudo apt-get autoremove -y
 
-sudo apt-get install \
-   ca-certificates \
-   curl \
-   gnupg \
-   lsb-release
+	read -p 'Press [Enter] key to continue...' W
+	
+	MainMenu
+}
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                        Add dockers official GPG key                         ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+function Docker {
+	clear
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                   DLT.GREEN AUTOMATIC DOCKER INSTALLATION                   ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	read -p 'Press [Enter] key to continue...' W
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                          Now set up the repository                          ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	cd /var/lib/hornet
+	docker-compose down
+	
+	sudo apt-get install jq -y
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║   Update and install packages to allow apt to use a repository over HTTPS   ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                            Install docker engine                            ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	sudo apt-get update
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
+	sudo apt-get install \
+		ca-certificates \
+		curl \
+		gnupg \
+		lsb-release
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                   Create hornet directory /var/lib/hornet                   ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                        Add dockers official GPG key                         ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-cd /var/lib
-mkdir hornet
-cd hornet
+	sudo mkdir -p /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                   Pull repo from iotaledger/hornet:develop                  ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                          Now set up the repository                          ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-wget -cO - https://github.com/iotaledger/hornet/releases/download/v2.0.0-alpha20/HORNET-2.0.0-alpha20-docker-example.tar.gz > install.tar.gz
+	echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "unpack:"
-tar -xzf install.tar.gz
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                            Install docker engine                            ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-echo "remove tar.gz:"
-rm -r install.tar.gz
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                               Set parameter                                 ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	read -p 'Press [Enter] key to continue...' W
 
-cd /var/lib/hornet
+	MainMenu
+}
 
-read -p 'Set domain-name: ' VAR_HORNET_HOST
-read -p 'Set mail for certificat renewal: ' VAR_ACME_EMAIL
-read -p 'Set dashboard username: ' VAR_USERNAME
-read -p 'Set password (blank): ' VAR_PASSWORD
+function ShimmerMainnet {
+	clear
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║            DLT.GREEN AUTOMATIC SHIMMER INSTALLATION WITH DOCKER             ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-credentials=$(docker-compose run --rm hornet tool pwd-hash --json --password $VAR_PASSWORD | sed -e 's/\r//g')
+	read -p 'Press [Enter] key to continue...' W
 
-VAR_DASHBOARD_PASSWORD=$(echo $credentials | jq -r '.passwordHash')
-VAR_DASHBOARD_SALT=$(echo $credentials | jq -r '.passwordSalt')
+	cd /var/lib/hornet
+	docker-compose down
 
-cd /var/lib/hornet
-rm .env
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                   Create hornet directory /var/lib/hornet                   ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-echo "ACME_EMAIL=$VAR_ACME_EMAIL" >> .env
-echo "HORNET_HOST=$VAR_HORNET_HOST" >> .env
-echo "DASHBOARD_HOST=dashboard.$VAR_HORNET_HOST" >> .env
-echo "DASHBOARD_USERNAME=$VAR_USERNAME" >> .env
-echo "DASHBOARD_PASSWORD=$VAR_DASHBOARD_PASSWORD" >> .env
-echo "DASHBOARD_SALT=$VAR_DASHBOARD_SALT" >> .env
-echo "GRAFANA_HOST=grafana.$VAR_HORNET_HOST" >> .env
+	cd /var/lib
+	mkdir hornet
+	cd hornet
 
-echo '      - "--dashboard.auth.username=${DASHBOARD_USERNAME}"' >> docker-compose.yml
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                   Pull repo from iotaledger/hornet:develop                  ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-sed "/alias/s/node/$VAR_HORNET_HOST/g" config.json > config_tmp.json
-mv config_tmp.json config.json
+	wget -cO - https://github.com/iotaledger/hornet/releases/download/v2.0.0-alpha20/HORNET-2.0.0-alpha20-docker-example.tar.gz > install.tar.gz
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                               Prepare docker                                ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	echo "unpack:"
+	tar -xzf install.tar.gz
 
-cd /var/lib/hornet/
-./prepare_docker.sh
+	echo "remove tar.gz:"
+	rm -r install.tar.gz
 
-echo ""
-echo "╔═════════════════════════════════════════════════════════════════════════════╗"
-echo "║                                Start Hornet                                 ║"
-echo "╚═════════════════════════════════════════════════════════════════════════════╝"
-echo ""
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                               Set parameter                                 ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
 
-cd /var/lib/hornet/
-docker-compose up -d
-docker exec -it grafana grafana-cli admin reset-admin-password $VAR_PASSWORD
+	cd /var/lib/hornet
 
-echo ""
-echo "═══════════════════════════════════════════════════════════════════════════════"
-echo " Hornet Dashboard: https://dashboard.$VAR_HORNET_HOST"
-echo " Hornet Password: <set during install>"
-echo " Grafana Dashboard: https://grafana.$VAR_HORNET_HOST"
-echo " Grafana Username: admin"
-echo " Grafana Password: <same as hornet password>"
-echo "═══════════════════════════════════════════════════════════════════════════════"
-echo ""
+	read -p 'Set domain-name: ' VAR_HORNET_HOST
+	read -p 'Set mail for certificat renewal: ' VAR_ACME_EMAIL
+	read -p 'Set dashboard username: ' VAR_USERNAME
+	read -p 'Set password (blank): ' VAR_PASSWORD
+
+	credentials=$(docker-compose run --rm hornet tool pwd-hash --json --password $VAR_PASSWORD | sed -e 's/\r//g')
+
+	VAR_DASHBOARD_PASSWORD=$(echo $credentials | jq -r '.passwordHash')
+	VAR_DASHBOARD_SALT=$(echo $credentials | jq -r '.passwordSalt')
+
+	cd /var/lib/hornet
+	rm .env
+
+	echo "ACME_EMAIL=$VAR_ACME_EMAIL" >> .env
+	echo "HORNET_HOST=$VAR_HORNET_HOST" >> .env
+	echo "DASHBOARD_HOST=dashboard.$VAR_HORNET_HOST" >> .env
+	echo "DASHBOARD_USERNAME=$VAR_USERNAME" >> .env
+	echo "DASHBOARD_PASSWORD=$VAR_DASHBOARD_PASSWORD" >> .env
+	echo "DASHBOARD_SALT=$VAR_DASHBOARD_SALT" >> .env
+	echo "GRAFANA_HOST=grafana.$VAR_HORNET_HOST" >> .env
+
+	echo '      - "--dashboard.auth.username=${DASHBOARD_USERNAME}"' >> docker-compose.yml
+
+	sed "/alias/s/node/$VAR_HORNET_HOST/g" config.json > config_tmp.json
+	mv config_tmp.json config.json
+
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                               Prepare docker                                ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
+
+	cd /var/lib/hornet/
+	./prepare_docker.sh
+
+	echo ""
+	echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+	echo "║                                Start Hornet                                 ║"
+	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+	echo ""
+
+	cd /var/lib/hornet/
+	docker-compose up -d
+	docker exec -it grafana grafana-cli admin reset-admin-password $VAR_PASSWORD
+
+	echo ""
+	echo "═══════════════════════════════════════════════════════════════════════════════"
+	echo " Hornet Dashboard: https://dashboard.$VAR_HORNET_HOST"
+	echo " Hornet Password: <set during install>"
+	echo " Grafana Dashboard: https://grafana.$VAR_HORNET_HOST"
+	echo " Grafana Username: admin"
+	echo " Grafana Password: <same as hornet password>"
+	echo "═══════════════════════════════════════════════════════════════════════════════"
+	echo ""
+
+	read -p 'Press [Enter] key to continue...' W
+	
+	MainMenu
+}
+
+rm -r test.sh
+
+MainMenu
