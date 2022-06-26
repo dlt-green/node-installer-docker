6ae4cb292c82a33dd4c48a0014254649a51a985d
bogenlos <tito.bogenlos@gmail.com>
2022-06-26 17:12:14 +0000

    Add tool to generate password hash and salt non-interactively.

---
+++ bee/tools/password_scriptable.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect -f
+
+# As Bee password tool is interactive this script enables to fetch passwort salt and hash.
+# Requirements: 'expect' must be installed on system
+
+set PASSWORD [lindex $argv 0]
+
+set timeout -1
+spawn ./password.sh $PASSWORD
+
+expect "Password: "
+send -- "$PASSWORD\n"
+
+expect "Re-enter password: "
+send -- "$PASSWORD\n"
+
+expect eof
