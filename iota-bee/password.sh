9796a5cb7a440bfce39fcf02378ba318befc500d
bogenlos <tito.bogenlos@gmail.com>
2022-06-28 17:42:28 +0000

    Bugfix in password.sh

--- iota-bee/password.sh
+++ iota-bee/password.sh
@@ -5,7 +5,10 @@ if [ -z $1 ]; then
 fi
 
 output=$(cd tools; ./password_scriptable.sh $1)
-passwordHash=$(echo -e "$output" | grep 'Password hash' | cut -d ' ' -f 3)
-passwordSalt=$(echo -e "$output" | grep 'Password salt' | cut -d ' ' -f 3)
+passwordHash=$(echo -e "$output" | grep 'Password hash' | cut -d ' ' -f 3 | tr -d '\r')
+passwordSalt=$(echo -e "$output" | grep 'Password salt' | cut -d ' ' -f 3 | tr -d '\r')
 
-jq --null-input --arg passwordHash "$passwordHash" --arg passwordSalt "$passwordSalt" '{"passwordHash": $passwordHash, "passwordSalt": $passwordSalt}'
+jq --null-input \
+   --arg passwordHash "$passwordHash" \
+   --arg passwordSalt "$passwordSalt" \
+   '{"passwordHash": $passwordHash, "passwordSalt": $passwordSalt}'
