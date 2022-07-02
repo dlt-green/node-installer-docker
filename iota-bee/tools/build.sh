57f21f05ce63c4181d7cb11700b09fa5d9084f66
bogenlos <tito.bogenlos@gmail.com>
2022-07-02 16:46:29 +0000

    Add build_win.sh to build install.tar.gz on windows (in alpine docker container)

--- iota-bee/tools/build.sh
+++ iota-bee/tools/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mkdir -p ../build
-tar -pcz --exclude='.env' --exclude='build' --exclude='build_tar.sh' -f ../build/install.tar.gz *
+mkdir -p ../build && tar -pcz --exclude='.env' --exclude='build' --exclude='build_tar.sh' --exclude='data' -f ../build/install.tar.gz ../*
