f8fac258058276478f8213b156b8181dab9eaacd
bogenlos <tito.bogenlos@gmail.com>
2022-08-03 09:21:12 +0000

    Build wasp:beta from master branch and improve build script.

--- build_packages.sh
+++ build_packages.sh
@@ -7,7 +7,6 @@ HORNET_VERSION=1.2.1
 WASP_VERSION=0.2.5
 
 build_node () {
-  # param1: node to build
   node=$1
   sourceDir=./$node
 
@@ -26,37 +25,59 @@ build_node () {
   echo "$node.tar.gz built successfully"
 }
 
-build_wasp_image () {
-  IMAGE_TAG=dltgreen/iota-wasp:$WASP_VERSION
-  BUILD_DIR_WASP=$BUILD_DIR/tmp_wasp
-
-  mkdir -p $BUILD_DIR_WASP
-  (cd $BUILD_DIR_WASP; curl -L -o wasp.tar.gz https://github.com/iotaledger/wasp/archive/refs/tags/v$WASP_VERSION.tar.gz; tar -xvf wasp.tar.gz --strip 1)
-
-  if [ -f $BUILD_DIR_WASP/Dockerfile ]; then
-    (cd $BUILD_DIR_WASP; docker build -t $IMAGE_TAG .)
-  fi
-
-  docker save $IMAGE_TAG > $BUILD_DIR/iota-wasp-v$WASP_VERSION.tar
-  rm -Rf $BUILD_DIR_WASP
-}
-
 build_hornet_image () {
-  IMAGE_TAG=dltgreen/iota-hornet:$HORNET_VERSION
-  BUILD_DIR_HORNET=$BUILD_DIR/tmp_hornet
+  imageName=dltgreen/iota-hornet:$HORNET_VERSION
+  buildDirHornet=$BUILD_DIR/tmp_hornet
 
-  mkdir -p $BUILD_DIR_HORNET
-  (cd $BUILD_DIR_HORNET; curl -L -o hornet.tar.gz https://github.com/iotaledger/hornet/archive/refs/tags/v${HORNET_VERSION}.tar.gz; tar -xvf hornet.tar.gz --strip 1)
+  mkdir -p $buildDirHornet
+  #(cd $buildDirHornet; curl -L -o hornet.tar.gz https://github.com/iotaledger/hornet/archive/refs/tags/v${HORNET_VERSION}.tar.gz; tar -xvf hornet.tar.gz --strip 1)
+  (cd $BUILD_DIR; git clone https://github.com/iotaledger/hornet.git tmp_hornet; cd tmp_hornet; git checkout v${HORNET_VERSION})
 
-  if [ -f $BUILD_DIR_HORNET/docker/Dockerfile ]; then
-    (cd $BUILD_DIR_HORNET; docker build -f docker/Dockerfile -t $IMAGE_TAG .)
+  if [ -f $buildDirHornet/docker/Dockerfile ]; then
+    (cd $buildDirHornet; docker build -f docker/Dockerfile -t $imageName .)
   fi
 
-  docker save $IMAGE_TAG > $BUILD_DIR/iota-hornet-v$HORNET_VERSION.tar
-  rm -Rf $BUILD_DIR_HORNET
+  docker save $imageName > $BUILD_DIR/iota-hornet-v$HORNET_VERSION.tar
+  rm -Rf $buildDirHornet
+
+  push_docker_image $imageName
 }
 
-upload () {
+build_wasp_image () {
+  local repoTag=$1
+  local imageTag=$2
+
+  local imageName=dltgreen/iota-wasp:$imageTag
+  local buildDirWasp=$BUILD_DIR/tmp_wasp
+
+  rm -Rf $buildDirWasp && mkdir -p $buildDirWasp
+  (cd $BUILD_DIR; git clone https://github.com/iotaledger/wasp.git tmp_wasp; cd tmp_wasp; git checkout $repoTag)
+
+  if [ -f $buildDirWasp/Dockerfile ]; then
+    (cd $buildDirWasp; docker build -t $imageName .)
+  fi
+
+  docker save $imageName > $BUILD_DIR/iota-wasp-$repoTag.tar
+  rm -Rf $buildDirWasp
+
+  push_docker_image $imageName
+}
+
+push_docker_image () {
+  local imageName=$1
+  
+  print_line
+  read -p "Push docker image to dockerhub? (y/n) " yn
+  echo ""
+  case $yn in 
+    y) docker push $imageName
+       ;;
+    *) echo "Image has not been pushed"
+       ;;
+  esac
+}
+
+upload_build_artefacts () {
   envFile=$(dirname "$0")/.env
   if [ ! -e "$envFile" ]; then
     echo "Missing .env!"
@@ -72,67 +93,132 @@ print_line () {
   echo "--------------------------------------------------------------------------------"
 }
 
-print_menu () {
+enter_to_continue () {
   print_line
-  read -p 'Press [Enter] key to continue...' W
-  clear
-  menu
+  echo $fl; read -p 'Press [Enter] key to continue... Press [STRG+C] to cancel...' W; echo $xx
 }
 
-menu () {
-  PS3="Enter a number: "
-  select opt in iota-hornet iota-hornet-dockerimage iota-bee iota-goshimmer iota-wasp iota-wasp-dockerimage upload clean quit; do
-    case $opt in
-      iota-hornet-dockerimage)
-        print_line
-        build_hornet_image
-        print_menu
-        ;;
-      iota-hornet)
-        print_line
-        build_node $opt
-        print_menu
-        ;;
-      iota-bee)
-        print_line
-        build_node $opt
-        print_menu
-        ;;
-      iota-goshimmer)
-        print_line
-        build_node $opt
-        print_menu
-        ;;
-      iota-wasp)
-        print_line
-        build_node $opt
-        print_menu
-        ;;
-      iota-wasp-dockerimage)
-        print_line
-        build_wasp_image
-        print_menu
-        ;;
-      upload)
-        print_line
-        upload
-        print_menu
-        ;;
-      clean)
-        print_line
-        rm -Rf $BUILD_DIR
-        echo "$BUILD_DIR deleted"
-        print_menu
-        ;;
-      quit)
-        exit 0
-        ;;
-      *) 
-        echo "Invalid option $REPLY"
-        ;;
-    esac
+print_menu () {
+  local longestArgumentLength=0
+  for item in "$@"; do
+      if [ ${#item} -gt $longestArgumentLength ]; then
+        longestArgumentLength=${#item}
+      fi
   done
+
+  local argumentsCount=${#@}
+  local longestInput=$(($argumentsCount - 1))
+  local menuInnerWidth=$(($longestArgumentLength + 20 + 2 + ${#longestInput}))
+  local innerLine=$(seq 1 $menuInnerWidth | sed 's/.*/═/' | tr -d '\n')
+  local innerBlanks=$(seq 1 $menuInnerWidth | sed 's/.*/ /' | tr -d '\n')
+
+	clear
+	echo ""
+  echo "╔$innerLine╗"
+  echo "║$innerBlanks║"
+
+  local iterator=1
+  for item in "$@"; do
+    local input="$iterator"
+    if [ $# -eq $iterator ]; then
+      input="X"
+      echo "║$innerBlanks║"
+    fi
+
+    local paddingLeftNum=$((${#longestInput} - ${#input}))
+    local paddingLeft=$(seq 1 $((10 + $paddingLeftNum)) | sed 's/.*/ /' | tr -d '\n')
+    local paddingRight=$(seq 1 $(($menuInnerWidth - 10 - $paddingLeftNum - ${#input} - 2 - ${#item})) | sed 's/.*/ /' | tr -d '\n')
+
+    echo "║${paddingLeft}${input}. ${item}${paddingRight}║"
+    iterator=$(($iterator + 1))
+  done
+
+  echo "║$innerBlanks║"
+  echo "╚$innerLine╝"
+  echo ""
+	echo "Select menu item: "
+	echo ""
 }
 
-clear
-menu
+MainMenu() {
+  print_menu "Docker images" "Node packages" "Build management" "Exit"
+	read  -p '> ' n
+	case $n in
+	1) DockerImagesMenu ;;
+	2) NodePackagesMenu ;;
+	3) BuildManagementMenu ;;
+	*) clear; exit ;;
+	esac
+}
+
+DockerImagesMenu() {
+  print_menu "iota-hornet ($HORNET_VERSION)" "iota-wasp ($WASP_VERSION)" "iota-wasp (beta)" "Back"
+	read  -p '> ' n
+	case $n in
+	1) print_line
+     build_hornet_image
+     enter_to_continue
+     DockerImagesMenu
+     ;;
+	2) print_line
+     build_wasp_image "v$WASP_VERSION" "$WASP_VERSION"
+     enter_to_continue
+     DockerImagesMenu
+     ;;
+	3) print_line
+     build_wasp_image "master" "beta"
+     enter_to_continue
+     DockerImagesMenu
+     ;;
+	*) MainMenu ;;
+	esac
+}
+
+NodePackagesMenu() {
+  print_menu "iota-hornet" "iota-bee" "iota-goshimmer" "iota-wasp" "Back"
+	read  -p '> ' n
+	case $n in
+	1) print_line
+     build_node "iota-hornet"
+     enter_to_continue
+	   NodePackagesMenu
+     ;;
+	2) print_line
+     build_node "iota-bee"
+     enter_to_continue
+	   NodePackagesMenu
+     ;;
+  3) print_line
+     build_node "iota-goshimmer"
+     enter_to_continue
+	   NodePackagesMenu
+     ;;
+  4) print_line
+     build_node "iota-wasp"
+     enter_to_continue
+	   NodePackagesMenu
+     ;;
+	*) MainMenu ;;
+	esac
+}
+
+BuildManagementMenu() {
+  print_menu "Clean build dir" "Upload build artefacts" "Back"
+	read  -p '> ' n
+	case $n in
+	1) print_line
+     rm -Rf $BUILD_DIR && mkdir -p $BUILD_DIR
+     echo "$BUILD_DIR cleaned"
+     enter_to_continue
+     BuildManagementMenu
+     ;;
+	2) print_line
+     upload_build_artefacts
+     enter_to_continue
+     BuildManagementMenu
+     ;;
+	*) MainMenu ;;
+	esac
+}
+
+MainMenu
