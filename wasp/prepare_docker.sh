37d9ba0387bb5ba02a458a7c11a1d0b387c52f32
bogenlos <tito.bogenlos@gmail.com>
2022-08-06 14:47:36 +0000

    Fix wasp username/password config for version 0.3.0

--- wasp/prepare_docker.sh
+++ wasp/prepare_docker.sh
@@ -26,13 +26,19 @@ echo "Adapting config with values from .env..."
 set_config $configPath ".database.directory"      "\"/app/waspdb\""
 set_config $configPath ".nanomsg.port"            "${WASP_NANO_MSG_PORT:-5550}"
 set_config $configPath ".peering.port"            "${WASP_PEERING_PORT:-4000}"
-set_config $configPath ".dashboard.auth.username" "\"${DASHBOARD_USERNAME:-wasp}\""
-set_config $configPath ".dashboard.auth.password" "\"${DASHBOARD_PASSWORD:-wasp}\""
 set_config $configPath ".logger.outputPaths"      "[\"stdout\"]"
 
-set_config_if_field_exists $configPath ".nodeconn.address" "\"${WASP_LEDGER_CONNECTION:?WASP_LEDGER_CONNECTION is mandatory}\""
-set_config_if_field_exists $configPath ".l1.apiAddress"    "\"${WASP_LEDGER_CONNECTION:?WASP_LEDGER_CONNECTION is mandatory}\""
-set_config_if_field_exists $configPath ".l1.faucetAddress" "\"${WASP_LEDGER_CONNECTION:?WASP_LEDGER_CONNECTION is mandatory}\""
+# wasp 0.2.5
+set_config_if_field_exists $configPath ".dashboard.auth.username" "\"${DASHBOARD_USERNAME:-wasp}\""
+set_config_if_field_exists $configPath ".dashboard.auth.password" "\"${DASHBOARD_PASSWORD:-wasp}\""
+# wasp 0.3.0
+move_rename_config         $configPath ".users.wasp"                                 ".users.${DASHBOARD_USERNAME:-wasp}"
+set_config_if_field_exists $configPath ".users.${DASHBOARD_USERNAME:-wasp}.password" "\"${DASHBOARD_PASSWORD:-wasp}\""
+set_config_if_field_exists $configPath ".webapi.auth.basic.username"                 "\"${DASHBOARD_USERNAME:-wasp}\""
+set_config_if_field_exists $configPath ".dashboard.auth.basic.username"              "\"${DASHBOARD_USERNAME:-wasp}\""
+set_config_if_field_exists $configPath ".nodeconn.address"                           "\"${WASP_LEDGER_CONNECTION:?WASP_LEDGER_CONNECTION is mandatory}\""
+set_config_if_field_exists $configPath ".l1.apiAddress"                              "\"${WASP_LEDGER_CONNECTION:?WASP_LEDGER_CONNECTION is mandatory}\""
+set_config_if_field_exists $configPath ".l1.faucetAddress"                           "\"${WASP_LEDGER_CONNECTION:?WASP_LEDGER_CONNECTION is mandatory}\""
 rm -f $tmp
 
 echo "Finished"
