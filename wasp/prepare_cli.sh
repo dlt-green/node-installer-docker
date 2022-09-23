eb466958e32ccf4235d449451b6254473d8010d2
bogenlos <tito.bogenlos@gmail.com>
2022-09-23 13:30:46 +0000

    Remove wasp-cli.json on uninstall

--- wasp/prepare_cli.sh
+++ wasp/prepare_cli.sh
@@ -25,6 +25,7 @@ if ! is_elevated_to_root; then
     echo "Deleting alias in $bashAliases..."
     sed -i '/# DLT.GREEN WASP-CLI/d' "$bashAliases" && \
     sed -i '/alias wasp-cli=/d' "$bashAliases" && \
+    sudo rm -f "$configPath"
     echo "  success"
     exit 0
   else
