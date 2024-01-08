c6a7557c916ceb531eb90a5d8138579caf0159db
dlt.green <contact@dlt.green>
2024-01-08 06:19:05 +0100

    +fix maintenance

--- node-installer.sh
+++ node-installer.sh
@@ -1849,8 +1849,8 @@ SystemMaintenance() {
 	      if [ -f docker-compose.yml ]; then
 	        if [ "$($NODE 2>&1 | grep 'iota')" ]; then docker network create iota >/dev/null 2>&1; fi
 	        if [ "$($NODE 2>&1 | grep 'shimmer')" ]; then docker network create shimmer >/dev/null 2>&1; fi
-	        docker compose pull
-	        /prepare_docker.sh >/dev/null 2>&1
+	        docker compose pull >/dev/null 2>&1
+	        ./prepare_docker.sh >/dev/null 2>&1
 	        docker compose up --no-start
 	      fi
 	    fi
