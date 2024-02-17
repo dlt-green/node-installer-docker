ad25d1956a4ffa64a1a9971ad933b7da22db145c
dlt.green <contact@dlt.green>
2024-02-17 11:18:35 +0100

    update entry node (autopeering)

--- node-installer.sh
+++ node-installer.sh
@@ -3360,7 +3360,7 @@ ShimmerCore() {
 		echo "IOTA_CORE_GOSSIP_PORT=15600" >> .env
 		echo "IOTA_CORE_AUTOPEERING_PORT=14626" >> .env
 		echo "IOTA_CORE_RUN_AS_ENTRY_NODE=false" >> .env
-		echo "IOTA_CORE_ENTRY_NODE=\"/dns/iota-core-testnet.dlt.green/udp/14626/autopeering/12D3KooWLPWi29KgYxCrYudVjkVRyfQXDLskb3JQgy5wUf8zzwB9\"" >> .env
+		echo "IOTA_CORE_ENTRY_NODE=\"/dns/iota-core-testnet.dlt.green/tcp/15600/12D3KooWLPWi29KgYxCrYudVjkVRyfQXDLskb3JQgy5wUf8zzwB9\"" >> .env
 
 		if [ "$VAR_CERT" = 0 ]
 		then
