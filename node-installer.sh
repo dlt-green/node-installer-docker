850b5c7a708a14ed5d47f2de9649a57a567b911d
dlt.green <contact@dlt.green>
2024-03-06 22:49:59 +0100

    installer: fix typo

--- node-installer.sh
+++ node-installer.sh
@@ -976,8 +976,8 @@ Dashboard() {
 	echo "╚═════════════════════════════════════════════════════════════════════════════╝"
 	if [ "$(crontab -l | grep "$VAR_CRON_URL" | grep "$VAR_CRON_JOB_2m\|$VAR_CRON_JOB_2u")" ]; then
 	  echo "$gr""              maintenance: ""$(printf '%02d' "$(crontab -l | grep "$VAR_CRON_URL" | grep "$VAR_CRON_JOB_2m\|$VAR_CRON_JOB_2u" | cut -d ' ' -f 2)")"":""$(printf '%02d' "$(crontab -l | grep "$VAR_CRON_URL" | grep "$VAR_CRON_JOB_2m\|$VAR_CRON_JOB_2u" | cut -d ' ' -f 1)")"" | day: ""$(crontab -l | grep "$VAR_CRON_URL" | grep "$VAR_CRON_JOB_2m\|$VAR_CRON_JOB_2u" | cut -d ' ' -f 3)"" | month: ""$(crontab -l | grep "$VAR_CRON_URL" | grep "$VAR_CRON_JOB_2m\|$VAR_CRON_JOB_2u" | cut -d ' ' -f 4)"" | weekday: ""$(crontab -l | grep "$VAR_CRON_URL" | grep "$VAR_CRON_JOB_2m\|$VAR_CRON_JOB_2u" | cut -d ' ' -f 5)""$xx"
+	  echo ""
 	else echo ""; fi
-	echo ""
 	echo "select menu item:"
 
 	if [ "$opt_mode" = 'd' ]; then
