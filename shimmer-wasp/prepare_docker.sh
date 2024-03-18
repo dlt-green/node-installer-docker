0baab8c0cfd132ae1732efdda0f4197aa97566b0
Tito Bogenlos <tito.bogenlos@gmail.com>
2024-03-18 20:46:56 +0000

    shimmer-wasp: Add configuration for chain address, add peering nodes to accessNodes field and configurable log level.

--- shimmer-wasp/prepare_docker.sh
+++ shimmer-wasp/prepare_docker.sh
@@ -14,11 +14,12 @@ configPath="${dataDir}/config/${configFilename}"
 usersConfigFilename="users.json"
 usersConfigPath="${dataDir}/config/${usersConfigFilename}"
 trustedPeersPath="${dataDir}/waspdb/trusted_peers.json"
+chainRegistryPath="${dataDir}/waspdb/chains/chain_registry.json"
 
 validate_ssl_config "WASP_SSL_CERT" "WASP_SSL_KEY"
 copy_common_assets
 
-prepare_data_dir "${dataDir}" "config" "waspdb"
+prepare_data_dir "${dataDir}" "config" "waspdb" "waspdb/chains"
 
 create_docker_network "shimmer"
 
@@ -30,6 +31,7 @@ curl -L -s -o "${configPath}" "${configUrl}"
 echo "Adapting config with values from .env..."
 sed -i 's|"waspdb/|"/app/waspdb/|g' "${configPath}"
 set_config "${configPath}" ".logger.outputPaths"            "[\"stdout\"]"
+set_config "${configPath}" ".logger.level"                  "\"${WASP_LOG_LEVEL:-info}\""
 set_config "${configPath}" ".inx.address"                   "\"hornet:9029\""
 set_config "${configPath}" ".p2p.identity.privateKey"       "\"${WASP_IDENTITY_PRIVATE_KEY}\""
 set_config "${configPath}" ".peering.port"                  "${WASP_PEERING_PORT:-4000}"
@@ -52,10 +54,14 @@ set_config "${usersConfigPath}" ".users.users[\"${DASHBOARD_USERNAME:-wasp}\"].p
 
 echo "Configure trusted peers..."
 if [ ! -f "${trustedPeersPath}" ]; then echo "{\"trustedPeers\": []}" > "${trustedPeersPath}"; fi
+if [ ! -f "${chainRegistryPath}" ]; then echo "{\"chainRecords\": [{\"chainID\": \"\", \"active\": false}]}" > "${chainRegistryPath}"; fi
 if ! grep -q -E "^WASP_TRUSTED_NODE_[0-9]+_URL" .env; then
   echo "  Skipped: No trusted peers defined in .env"
 else
   jq '.trustedPeers |= map(select(.name == "me"))' "${trustedPeersPath}" > "${trustedPeersPath}.tmp" && mv -f "${trustedPeersPath}.tmp" "${trustedPeersPath}"
+  set_config "${chainRegistryPath}" ".chainRecords[0].chainID" "\"${WASP_CHAIN_ADDRESS:-smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s}\""
+  set_config "${chainRegistryPath}" ".chainRecords[0].accessNodes" "[]"
+
   grep -E "^WASP_TRUSTED_NODE_[0-9]+_URL" .env | sort | while IFS= read -r trustedNodeUrl; do
     trustedNodeNumber=$(echo "${trustedNodeUrl}" | cut -d '_' -f 4)
     name=$(grep -E "WASP_TRUSTED_NODE_${trustedNodeNumber}_NAME" .env | cut -d '=' -f 2)
@@ -67,9 +73,13 @@ else
     jq -r --arg name "${name}" --arg url "${url}" --arg pubKey "${pubKey}" \
       '.trustedPeers += [{name: $name, publicKey: $pubKey, peeringURL: $url}]' \
       "${dataDir}/waspdb/trusted_peers.json" > "${trustedPeersPath}.tmp" && mv -f "${trustedPeersPath}.tmp" "${trustedPeersPath}"
+
+    jq -r --arg pubKey "${pubKey}" \
+      '.chainRecords[0].accessNodes += [$pubKey]' \
+      "${chainRegistryPath}" > "${chainRegistryPath}.tmp" && mv -f "${chainRegistryPath}.tmp" "${chainRegistryPath}"
   done
 fi
-chown 65532:65532 "${trustedPeersPath}"
+chown 65532:65532 "${trustedPeersPath}" "${chainRegistryPath}"
 
 echo ""
 echo "Finished"
