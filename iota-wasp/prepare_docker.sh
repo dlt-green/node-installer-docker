667973e5dfe6aeeba9ab0a768d971ac2729ca8d6
Tito Bogenlos <tito.bogenlos@gmail.com>
2024-04-05 06:54:57 +0000

    iota-wasp: Bring over changes from shimmer-wasp

--- iota-wasp/prepare_docker.sh
+++ iota-wasp/prepare_docker.sh
@@ -14,11 +14,12 @@ configPath="${dataDir}/config/${configFilename}"
 usersConfigFilename="users.json"
 usersConfigPath="${dataDir}/config/${usersConfigFilename}"
 trustedPeersPath="${dataDir}/waspdb/trusted_peers.json"
+chainRegistryPath="${dataDir}/waspdb/chains/chain_registry.json"
 
 validate_ssl_config "WASP_SSL_CERT" "WASP_SSL_KEY"
 copy_common_assets
 
-prepare_data_dir "${dataDir}" "config" "waspdb"
+prepare_data_dir "${dataDir}" "config" "waspdb" "waspdb/chains"
 
 create_docker_network "iota"
 
@@ -29,16 +30,20 @@ curl -L -s -o "${configPath}" "${configUrl}"
 
 echo "Adapting config with values from .env..."
 sed -i 's|"waspdb/|"/app/waspdb/|g' "${configPath}"
-set_config "${configPath}" ".logger.outputPaths"            "[\"stdout\"]"
-set_config "${configPath}" ".inx.address"                   "\"hornet:9029\""
-set_config "${configPath}" ".p2p.identity.privateKey"       "\"${WASP_IDENTITY_PRIVATE_KEY}\""
-set_config "${configPath}" ".peering.port"                  "${WASP_PEERING_PORT:-4000}"
-set_config "${configPath}" ".peering.peeringURL"            "\"${WASP_HOST}:${WASP_PEERING_PORT:-4000}\""
-set_config "${configPath}" ".webapi.auth.scheme"            "\"${WASP_WEBAPI_AUTH_SCHEME:-jwt}\""
-set_config "${configPath}" ".webapi.auth.jwt.duration"      "\"${WASP_JWT_DURATION:-24h}\""
-set_config "${configPath}" ".webapi.auth.basic.username"    "\"${DASHBOARD_USERNAME:-wasp}\""
-set_config "${configPath}" ".prometheus.bindAddress"        "\"0.0.0.0:9312\""
-set_config "${configPath}" ".db.debugSkipHealthCheck"       "${WASP_DEBUG_SKIP_HEALTH_CHECK:-false}"
+set_config "${configPath}" ".logger.outputPaths"                       "[\"stdout\"]"
+set_config "${configPath}" ".logger.level"                             "\"${WASP_LOG_LEVEL:-info}\""
+set_config "${configPath}" ".inx.address"                              "\"hornet:9029\""
+set_config "${configPath}" ".p2p.identity.privateKey"                  "\"${WASP_IDENTITY_PRIVATE_KEY}\""
+set_config "${configPath}" ".peering.port"                             "${WASP_PEERING_PORT:-4000}"
+set_config "${configPath}" ".peering.peeringURL"                       "\"${WASP_HOST}:${WASP_PEERING_PORT:-4000}\""
+set_config "${configPath}" ".webapi.auth.scheme"                       "\"${WASP_WEBAPI_AUTH_SCHEME:-jwt}\""
+set_config "${configPath}" ".webapi.auth.jwt.duration"                 "\"${WASP_JWT_DURATION:-24h}\""
+set_config "${configPath}" ".webapi.auth.basic.username"               "\"${DASHBOARD_USERNAME:-wasp}\""
+set_config "${configPath}" ".webapi.limits.confirmedStateLagThreshold" "10"
+set_config "${configPath}" ".prometheus.bindAddress"                   "\"0.0.0.0:9312\""
+set_config "${configPath}" ".db.debugSkipHealthCheck"                  "${WASP_DEBUG_SKIP_HEALTH_CHECK:-false}"
+#set_config "${configPath}" ".snapshots.localPath"                      "\"/app/waspdb/snap\""
+#set_config "${configPath}" ".snapshots.networkPaths"                   "\"https://???\""
 
 set_config_if_present_in_env "${configPath}" "WASP_PRUNING_MIN_STATES_TO_KEEP" ".stateManager.pruningMinStatesToKeep"
 
@@ -49,25 +54,10 @@ set_config "${usersConfigPath}" ".users.users[\"${DASHBOARD_USERNAME:-wasp}\"].p
 set_config "${usersConfigPath}" ".users.users[\"${DASHBOARD_USERNAME:-wasp}\"].permissions"  "[\"write\"]"
 
 echo "Configure trusted peers..."
-if [ ! -f "${trustedPeersPath}" ]; then echo "{\"trustedPeers\": []}" > "${trustedPeersPath}"; fi
-if ! grep -q -E "^WASP_TRUSTED_NODE_[0-9]+_URL" .env; then
-  echo "  Skipped: No trusted peers defined in .env"
-else
-  jq '.trustedPeers |= map(select(.name == "me"))' "${trustedPeersPath}" > "${trustedPeersPath}.tmp" && mv -f "${trustedPeersPath}.tmp" "${trustedPeersPath}"
-  grep -E "^WASP_TRUSTED_NODE_[0-9]+_URL" .env | sort | while IFS= read -r trustedNodeUrl; do
-    trustedNodeNumber=$(echo "${trustedNodeUrl}" | cut -d '_' -f 4)
-    name=$(grep -E "WASP_TRUSTED_NODE_${trustedNodeNumber}_NAME" .env | cut -d '=' -f 2)
-    if [ -z "${name}" ]; then name="peer${trustedNodeNumber}"; fi
-    url=$(grep -E "WASP_TRUSTED_NODE_${trustedNodeNumber}_URL" .env | cut -d '=' -f 2)
-    pubKey=$(grep -E "WASP_TRUSTED_NODE_${trustedNodeNumber}_PUBKEY" .env | cut -d '=' -f 2)
+configure_trusted_peers "${trustedPeersPath}"
 
-    echo "  Adding trusted peer ${name} (${url})"
-    jq -r --arg name "${name}" --arg url "${url}" --arg pubKey "${pubKey}" \
-      '.trustedPeers += [{name: $name, publicKey: $pubKey, peeringURL: $url}]' \
-      "${dataDir}/waspdb/trusted_peers.json" > "${trustedPeersPath}.tmp" && mv -f "${trustedPeersPath}.tmp" "${trustedPeersPath}"
-  done
-fi
-chown 65532:65532 "${trustedPeersPath}"
+#echo "Configure chain access nodes for IotaEVM..."
+#configure_chain_access_nodes "${chainRegistryPath}" "IotaEVM" "iota1..."
 
 echo ""
 echo "Finished"
